2512d152a12e7b8bcb57667491d298d16851e279
MarissaAG <747121@student.dusd.net>
2023-03-22 12:19:31 -0700

    player controller

--- Unit3Gutierrez/Assets/Scripts/PlayerController.cs
+++ Unit3Gutierrez/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        physics.gravity *= gravityModifier;
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
